db78b7c9d7a956a5ede7cfcfb4e81dc266bb0d16
Eric Lin <ericshenl2789@nycstudents.net>
2025-02-12 13:46:16 +0000

    small change:

--- src/main/java/com/example/project/Student/Utility.java
+++ src/main/java/com/example/project/Student/Utility.java
@@ -5,21 +5,18 @@ import java.util.Arrays;
 public class Utility {
     
     public static ArrayList<Student> sortStudents(ArrayList<Student> list){
-        boolean swapped = false;
         for(int i = 0; i <list.size() - 1; i++){
             for(int j = 0; j < list.size() - i - 1; j++){
                 if(list.get(j).getLastName().compareTo(list.get(j + 1).getLastName()) > 0){
                     Student temp = list.get(j);
                     list.set(j, list.get(j+1));
                     list.set(j+1, temp);
-                    swapped = true;
                 }
                 if(list.get(j).getLastName().equals(list.get(j+1).getLastName())){
                     if(list.get(j).getFirstName().compareTo(list.get(j + 1).getFirstName()) > 0){
                         Student temp = list.get(j);
                         list.set(j, list.get(j+1));
                         list.set(j+1, temp);
-                        swapped = true;
                     }
                 }
                 if(list.get(j).getFirstName().equals(list.get(j+1).getFirstName())){
@@ -27,12 +24,9 @@ public class Utility {
                         Student temp = list.get(j);
                         list.set(j, list.get(j+1));
                         list.set(j+1, temp);
-                        swapped = true;
                     }
                 }
-                if(!swapped){
-                    break;
-                }
+
             }
         }
         return list;
